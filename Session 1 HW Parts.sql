517946cf5c94966b444e50a3080dc1d09a3840ff
Camille Atlan <99365065+CamilleA3@users.noreply.github.com>
2023-03-27 21:08:46 -0400

    Add files via upload

---
+++ Session 1 HW Parts.sql
@@ -0,0 +1,15 @@
+create database Parts;
+use Parts;
+create table part(P_ID char(3), PNAME varchar(50), COLOR varchar(50), WEIGHT varchar(50), CITY varchar(50));
+create table project(J_ID char(3), JNAME varchar(50), CITY varchar(50));
+create table supplier(S_ID char(3), SNAME varchar(50), STATUS int, CITY varchar(50));
+create table supply(S_ID varchar(3), P_ID varchar(3), J_ID varchar(50), QUANTITY varchar(3));
+insert into part (P_ID, PNAME, COLOR, WEIGHT, CITY) values ('P1', 'NUT', 'RED', 12, 'LONDON'), ('P2', 'BOLT', 'GREEN', 17, 'PARIS'), ('P3', 'SCREW', 'BLUE', 17, 'ROME'), ('P4', 'SCREW', 'RED', 14, 'LONDON'), ('P5', 'CAM', 'BLUE', 12, 'PARIS'), ('P6', 'COG', 'RED', 19, 'LONDON');
+insert into project (J_ID, JNAME, CITY) values ('J1', 'SORTER', 'PARIS'), ('J2', 'DISPLAY', 'ROME'), ('J3', 'OCR', 'ATHENS'), ('J4', 'CONSOLE', 'ATHENS'), ('J5', 'RAID', 'LONDON'), ('J6', 'EDS', 'OSLO'), ('J7', 'TAPE', 'LONDON');
+insert into supplier (S_ID, SNAME, STATUS, CITY) values ('S1','SMITH', 20,'LONDON'), ('S2','JONES', 10,'PARIS'), ('S3','BLAKE', 30,'PARIS'), ('S4','CLARK', 20,'LONDON'), ('S5','ADAMS', 30,'ATHENS');
+alter table supply modify QUANTITY integer;
+insert into supply (S_ID, P_ID, J_ID, QUANTITY) values ('S1','P1','J1', 200), ('S1','P1','J4', 700), ('S2','P3','J1', 400), ('S2','P3','J2', 200), ('S2','P3','J3', 200), ('S2','P3','J4', 500), ('S2','P3','J5', 600), ('S2','P3','J6', 400), ('S2','P3','J7', 800), ('S2','P5','J2', 100), ('S3','P3','J1', 200), ('S3','P4','J2', 500), ('S4','P6','J3', 300), ('S4','P6','J7', 300), ('S5','P2','J2', 200), ('S5','P2','J4', 100), ('S5','P5','J5', 500), ('S5','P5','J7', 100), ('S5','P6','J2', 200), ('S5','P1','J4', 100), ('S5','P3','J4', 200), ('S5','P4','J4', 800), ('S5','P5','J4', 400), ('S5','P6','J4', 500);
+select distinct PNAME from Part;
+select * from project where city = 'LONDON';
+select PNAME, WEIGHT from part where COLOR = 'RED';
+select distinct SNAME from supplier where CITY = 'LONDON';
